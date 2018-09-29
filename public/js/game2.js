652e16e2f2fffc6b753529b518b4677c6060e60b
maribossi <marianne.bossema@gmail.com>
2018-09-29 15:01:50 +0200

    main and player screens

--- public/js/game2.js
+++ public/js/game2.js
@@ -1,37 +1,43 @@
 var socket;
-var r,g,b;
+var r, g, b;
+
 function setup() {
     createCanvas(800, 600);
-    background(51);
-
+    
     r = random(255);
     b = random(255);
     g = random(255);
 
-    socket = io.connect('http://localhost:8081')
-    socket.on('playerDraw', newDrawing);
-  }
-  
-  function draw() {
-    
-    
-  }
+    background(r,g,b);
 
-  function newDrawing(data) {
-    
+    socket = io.connect('http://localhost:8081')
+    socket.on('currentPlayers', showMainscreen);
+}
+
+function showMainscreen(data) {
+    background(0);
+    console.log("I am the main screen !!!")
+    socket.on('playerDraw', newDrawing);
+}
+
+
+function draw() {
+
+
+}
+
+function newDrawing(data) {
     noStroke();
-    fill(data.r,data.g,data.b);
-    ellipse(data.x, data.y, 36,36);
-    
-    
-  }
-  
-  function mouseDragged() {
-    console.log('Sending ' + mouseX + ',' + mouseY);
-    noStroke();
-    fill(r,g,b);
-    ellipse(mouseX, mouseY, 36,36);
-    
+    fill(data.r, data.g, data.b);
+    ellipse(data.x, data.y, 36, 36);
+}
+
+function mouseDragged() {
+
+    // noStroke();
+    // fill(r,g,b);
+    // ellipse(mouseX, mouseY, 36,36);
+
     var data = {
         x: mouseX,
         y: mouseY,
@@ -41,5 +47,5 @@ function setup() {
     }
 
     socket.emit('playerDraw', data);
-    
-  }
+
+}
--- server.js
+++ server.js
@@ -21,22 +21,25 @@ app.get('/', function (req, res) {
 });
 
 io.on('connection', function (socket) {
-  console.log('a user connected');
   // create a new player and add it to our players object
   players[socket.id] = {
-    playerId: socket.id,
-    color: Math.floor(Math.random() * 255)
+    playerId: socket.id
   };
-  // send the players object to the new player
-  socket.emit('currentPlayers', players);
-  // update all other players of the new player
-  socket.broadcast.emit('newPlayer', players[socket.id]);
-
+  console.log('a user connected ' + Object.keys(players).length);
+  // send the players object only to the MAIN player screen
+  if(Object.keys(players).length == 1){
+    socket.emit('currentPlayers', players);
+  }
+    
+    // update all other players of the new player
+    //socket.broadcast.emit('newPlayer', players[socket.id]);
+  
+  
   // when a player disconnects, remove them from our players object
   socket.on('disconnect', function () {
-    console.log('user disconnected');
     // remove this player from our players object
     delete players[socket.id];
+    console.log('user disconnected ' + Object.keys(players).length);
     // emit a message to all players to remove this player
     io.emit('disconnect', socket.id);
   });
